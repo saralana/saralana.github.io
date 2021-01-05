e9e7003cb17b2da19611fed34f42c6afe7c2799b
Sara Lana <saralgc@gmail.com>
2021-01-05 12:08:29 -0300

    Delete preload.js

--- js/preload.js
+++
@@ -1,5 +0,0 @@
-$(window).on('load', function () {
-    $('#preloader .inner').fadeOut();
-    $('#preloader').delay(350).fadeOut('slow'); 
-    $('body').delay(350).css({'overflow': 'visible'});
-})
