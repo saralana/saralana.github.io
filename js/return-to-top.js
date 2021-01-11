3895932be08113d514a835504612c55dff0c9622
Sara Lana <saralgc@gmail.com>
2021-01-10 22:46:26 -0300

    arrow to top and link color

--- js/return-to-top.js
+++ js/return-to-top.js
@@ -1,28 +1,3 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
-  });
-}
-
-
-function openNav() {
-  document.getElementById("sidenav").style.display = "block";
-}
-
-function closeNav() {
-  document.getElementById("sidenav").style.display = "none";
-}
-
-
 //Get the button
 var mybutton = document.getElementById("return-to-top");
 // When the user scrolls down 20px from the top of the document, show the button
